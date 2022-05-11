faf1d8f9fc77b1ed6dc8eec8e54d207d06f81456
NoWorryYun <dbstjd3201@naver.com>
2022-05-11 17:05:24 +0900

    4검색 풀긴했는데... 너무 야매식...

--- src/com/javaex/phonebook/PhonbookApp.java
+++ src/com/javaex/phonebook/PhonbookApp.java
@@ -20,6 +20,22 @@ public class PhonbookApp {
 		BufferedReader br = new BufferedReader(isr);
 		Scanner sc = new Scanner(System.in);
 
+		while (true) {
+			Person p = new Person();
+			String str = br.readLine();
+			if (str == null) {
+				break;
+			}
+			String[] pArray = str.split(",");
+			p.setName(pArray[0]);
+			p.setHp(pArray[1]);
+			p.setCompany(pArray[2]);
+			person.add(p);
+		}
+
+		Writer fw = new FileWriter("./PhoneDB.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+
 		System.out.println("********************************************");
 		System.out.println("************ 전화번호 관리 프로그램	************");
 		System.out.println("********************************************");
@@ -37,18 +53,6 @@ public class PhonbookApp {
 				System.out.println("*******************************************");
 				break;
 			} else if (num == 1) {
-				while (true) {
-					Person p = new Person();
-					String str = br.readLine();
-					if (str == null) {
-						break;
-					}
-					String[] pArray = str.split(",");
-					p.setName(pArray[0]);
-					p.setHp(pArray[1]);
-					p.setCompany(pArray[2]);
-					person.add(p);
-				}
 				for (int i = 0; i < person.size(); i++) {
 					System.out.print((i + 1) + ".   ");
 					person.get(i).showInfo();
@@ -79,36 +83,19 @@ public class PhonbookApp {
 				p.setCompany(sc.nextLine());
 
 				person.add(p);
-				Writer fw = new FileWriter("./PhoneDB.txt");
-				BufferedWriter bw = new BufferedWriter(fw);
 
 				for (Person s : person) {
 					String saveStr = s.getName() + "," + s.getHp() + "," + s.getCompany();
 					bw.write(saveStr);
 					bw.newLine();
 				}
-				bw.close();
 				System.out.println("[등록되었습니다.]");
 
 			} else if (num == 3) {
-				while (true) {
-					Person p = new Person();
-					String str = br.readLine();
-					if (str == null) {
-						break;
-					}
-					String[] pArray = str.split(",");
-					p.setName(pArray[0]);
-					p.setHp(pArray[1]);
-					p.setCompany(pArray[2]);
-					person.add(p);
-				}
 				System.out.println("<3.삭제>");
 				System.out.print(">번호 : ");
 				person.remove((sc.nextInt() - 1));
 
-				Writer wr = new FileWriter("./PhoneDB.txt");
-				BufferedWriter bw = new BufferedWriter(wr);
 				for (Person s : person) {
 					String saveStr = s.getName() + "," + s.getHp() + "," + s.getCompany();
 					bw.write(saveStr);
@@ -117,24 +104,22 @@ public class PhonbookApp {
 
 				System.out.println("삭제되었습니다.");
 
-				bw.close();
 			} else if (num == 4) {
 				System.out.println("<4.검색>");
-				String str = br.readLine();
-				char ch = str.charAt(0);
 				System.out.print("이름: ");
-				String name = sc.nextLine();
-				for (Person p : person) {
-					String kkk = p.getName();
-
-					char c1 = kkk.charAt(0);
-					char c2 = kkk.charAt(1);
-					char c3 = kkk.charAt(2);
-
-					if (ch == c1 || ch == c2 || ch == c3) {
-						p.showInfo();
+				sc.nextLine();
+				String typing = sc.nextLine();
+				char searcher = typing.charAt(0);
+				for (int i = 0; i < person.size(); i++) {
+					String[] pName = person.get(i).getName().split("");
+					
+					char search01 = pName[0].charAt(0);
+					char search02 = pName[1].charAt(0);
+					char search03 = pName[2].charAt(0);
+					
+					if (searcher == search01 || searcher == search02 || searcher == search03) {
+						person.get(i).showInfo();
 					}
-
 				}
 
 			} else {
@@ -144,7 +129,7 @@ public class PhonbookApp {
 			System.out.println("");
 
 		}
-
+		bw.close();
 		sc.close();
 		br.close();
 	}
